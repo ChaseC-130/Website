9bd726c5d96dfdba628a0b528a459d0355fa4b4c
Chase Cargill <chase.c.cargill@gmail.com>
2020-10-14 09:34:31 -0500

    Added Discord Bot and Fixed Server listening Port

--- express/dotascript.js
+++ express/dotascript.js
@@ -4,12 +4,17 @@ function Player(playerId){
 
 
 player1 = new Player(154691194);
-Players = [player1]
+player2 = new Player(108153045);
+player3 = new Player(101975774);
+player4 = new Player(81055475);
+
+
+Players = [player1, player2, player3, player4]
 
 function dotaScores() {
     let theExport = ""; 
     Players.forEach((player) => 
-    $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('https://www.dotabuff.com/players/154691194'), function (data) {
+    $.getJSON('https://api.allorigins.win/get?url=' + encodeURIComponent('https://www.dotabuff.com/players/' + player.playerId), function (data) {
     
     var avatar = (data.contents.match('(?<=ge\" content=\").+?(?=\" /><m)'))
     var name = (data.contents.match('(?<="Content-Type\" /><title>).+?(?= - Ov)'))
